26dd1cfa59770bf8b0fee8cb0b7e6782007da8f9
bertinus <paul.f.bertin@gmail.com>
2018-10-05 15:00:00 -0400

    Add split file

--- dataset.py
+++ dataset.py
@@ -6,27 +6,11 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 
 
-def one_hot_encoding(pathology):
-    """
-
-    :param pathology: String, name of the pathology
-    :return: function one hot that returns 1 if the row is labeled with pathology
-    """
-
-    def one_hot(row):
-        if pathology in row['Finding Labels']:
-            return 1
-        return 0
-
-    return one_hot
-
-
 class XrayDataset(Dataset):
 
     def __init__(self, datadir, csvpath, transform=None):
 
         self.datadir = datadir
-        self.csvpath = csvpath
         self.transform = transform
         self.pathologies = ["Atelectasis", "Consolidation", "Infiltration",
                             "Pneumothorax", "Edema", "Emphysema", "Fibrosis", "Effusion", "Pneumonia",
@@ -34,10 +18,6 @@ class XrayDataset(Dataset):
 
         # Load data
         self.Data = pd.read_csv(csvpath)
-
-        # Add one hot encodings
-        for pathology in self.pathologies:
-            self.Data[pathology] = self.Data.apply(one_hot_encoding(pathology), axis=1)
 
     def __len__(self):
         return len(self.Data)
@@ -48,6 +28,8 @@ class XrayDataset(Dataset):
         # Check that images are 2D arrays
         if len(im.shape) > 2:
             im = im[:, :, 0]
+        if len(im.shape) < 2:
+            print("error, dimension lower than 2 for image", self.Data['Image Index'][idx])
 
         # Add color channel
         im = im[:, :, None]
@@ -59,7 +41,7 @@ class XrayDataset(Dataset):
         return im, self.Data[self.pathologies].loc[idx].values
 
 
-def MyDataLoader(datadir, csvpath, inputsize):
+def MyDataLoader(datadir, csvpath, inputsize, batch_size=16):
     # Transformations
     data_transforms = transforms.Compose([
         transforms.ToPILImage(),
@@ -70,19 +52,19 @@ def MyDataLoader(datadir, csvpath, inputsize):
 
     # Initialize dataloader
     dataset = XrayDataset(datadir, csvpath, transform=data_transforms)
-    dataloader = DataLoader(dataset, shuffle=True, batch_size=16)
+    dataloader = DataLoader(dataset, shuffle=True, batch_size=batch_size)
 
     return dataloader
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     ####################################################################################################################
     # Test
     ####################################################################################################################
 
     datadir = "/home/user1/Documents/Data/ChestXray/images"
-    csvpath = "/home/user1/Documents/Data/ChestXray/Data_Entry_2017.csv"
+    csvpath = "/home/user1/Documents/Data/ChestXray/DataTrain.csv"
     inputsize = [224, 224]
 
     # Transformations
---
+++ split.py
@@ -0,0 +1,77 @@
+#%%
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+import numpy as np
+
+
+def one_hot_encoding(pathology):
+    """
+    :param pathology: String, name of the pathology
+    :return: function one hot that returns 1 if the row is labeled with pathology
+    """
+
+    def one_hot(row):
+        if pathology in row['Finding Labels']:
+            return 1
+        return 0
+
+    return one_hot
+
+
+#%%
+if __name__=="__main__":
+
+    ####################################################################################################################
+    # %% Build Train Test Val Datasets
+    ####################################################################################################################
+
+    testListPath = "/home/user1/Documents/Data/ChestXray/Utils/test_list.txt"
+    csvpath = "/home/user1/Documents/Data/ChestXray/Data_Entry_2017.csv"
+    savepath = "/home/user1/Documents/Data/ChestXray"
+
+
+    # Load data
+    Data = pd.read_csv(csvpath)
+    testList = pd.read_csv(testListPath, header=None)[0].tolist()
+
+
+    # Add one hot encodings
+    pathologies = ["Atelectasis", "Consolidation", "Infiltration",
+                        "Pneumothorax", "Edema", "Emphysema", "Fibrosis", "Effusion", "Pneumonia",
+                        "Pleural_thickening", "Cardiomegaly", "Nodule", "Mass", "Hernia"]
+    for pathology in pathologies:
+        Data[pathology] = Data.apply(one_hot_encoding(pathology), axis=1)
+
+
+    # Split Test vs TrainVal using the same splitting as the authors of the Dataset
+    DataTest = Data.loc[Data["Image Index"].isin(testList)]
+    DataTrainVal = Data.loc[~Data["Image Index"].isin(testList)]
+    DataTrainVal.reset_index(inplace=True)
+
+
+    # Split Train vs Validation on patient level
+    patients = DataTrainVal['Patient ID'].unique().tolist()
+    patients_train, patients_val = train_test_split(patients, test_size=0.11, random_state=14)
+
+    DataTrain = DataTrainVal.loc[DataTrainVal['Patient ID'].isin(patients_train)]
+    DataVal = DataTrainVal.loc[DataTrainVal['Patient ID'].isin(patients_val)]
+    Train_idx_list = DataTrain.index.tolist()
+    Val_idx_list = DataVal.index.tolist()
+
+    print("validation proportion :", DataVal.shape[0] / DataTrainVal.shape[0])
+
+    ####################################################################################################################
+    # %% Save as csv
+    ####################################################################################################################
+
+    # Save csv corresponding to train, test and validation
+    DataTrainVal.to_csv(os.path.join(savepath, "DataTrainVal.csv"), index=False)
+    DataTrain.to_csv(os.path.join(savepath, "DataTrain.csv"), index=False)
+    DataVal.to_csv(os.path.join(savepath, "DataVal.csv"), index=False)
+    np.save(os.path.join(savepath, "Train_Idx_List"), Train_idx_list)
+    np.save(os.path.join(savepath, "Val_Idx_List"), Val_idx_list)
+    DataTest.to_csv(os.path.join(savepath, "DataTest.csv"), index=False)
+
+
+
